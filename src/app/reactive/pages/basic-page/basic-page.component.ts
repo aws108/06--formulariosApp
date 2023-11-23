e10c94c52f87b47a7b1534c4fbfa5fc8ddcfc8b0
aws108 <sergiolizanagarcia@gmail.com>
2023-11-23 16:39:37 +0100

    add basic page valid form

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -32,9 +32,35 @@ export class BasicPageComponent implements OnInit{
     // this.myForm.reset(televisor) //5
   }
 
+  isValidField(field: string): boolean | null{
+     return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field: string): string | null{
+    if (!this.myForm.controls[field]){ // El formulario no contiene el name?
+      return null;
+    }
+    const errors = this.myForm.controls[field].errors || {}; //7 
+    for (const key of Object.keys(errors)){ //8 
+      // console.log(key) //9 
+      switch(key){
+        case 'required': 
+          return 'Este campo es obligatorio';
+        case 'minlength': 
+          return `El campo requiere mínimo ${errors['minlength'].requiredLength} carácteres.`;
+      }
+    }
+    return null;
+  }
+
   onSave(): void {
-    if(this.myForm.invalid){return;}
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched(); //6
+      return;
+    }
+
     console.log(this.myForm.value); 
+
     this.myForm.reset(); //4 
   }
 
@@ -47,3 +73,7 @@ export class BasicPageComponent implements OnInit{
 // 4-> resetea el formulario cuando ya has guardado. Una vez se hace, deja los campos a null. Si se quiere dejar como hace el profe, dejar el campo a 0:
 // this.myForm.reset({price: 0, inStorage: 0});
 // 5-> Inicializa el formulario. Eso se hace en el caso de que a una web le llegue info de algún sitio o backend y la tenga que mostrar en un formulario. 
+// 6-> Si el formulario es invalido, marca todos los campos como tocados, así saltan los errores
+// 7-> Si es nulo, devolvemos un objeto vacío
+// 8-> Sacas todos los errores de la const errors. Devuelve un array de errores
+// 9-> Si haces click en el campo y quitas el focus, sale que el error es del required
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required, Validators.minLength(3)],
+    favouriteGames: this.fb.array([//array de juegos
+      ['Metal Gear', Validators.required], //Valores iniciales
+      ['F1 championship', Validators.required],
+    ]) 
+  })
+
+  constructor(private fb: FormBuilder){}
+  
+  // get favouriteGamesControls(){ //Manera 1 del HTML
+  //   return this.myForm.get('favouriteGames') as FormArray;
+  // }
+
+  onSubmit(): void{
+    if( this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value)
+    this.myForm.reset();
+  }
 }
