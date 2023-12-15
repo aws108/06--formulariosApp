1afd73b9bc1c29ff76eab85c7788a2ccac2f8d73
aws108 <sergiolizanagarcia@gmail.com>
2023-12-15 16:43:28 +0100

    switches page.. doing register-page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required] ],
+    email: ['', [Validators.required] ],
+    username: ['', [Validators.required] ],
+    password: ['', [Validators.required, Validators.minLength(6)] ],
+    password2: ['', [Validators.required] ],
+  });
+
+  constructor(private fb: FormBuilder){}
+
+  onSubmit(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
+
+  isValidField(field: string): any{
+
+ }
 
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
 })
 export class SwitchesPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue], // debe ser true para ser validado
+  })
+
+  person = {};
+
+  constructor(private fb: FormBuilder){}
+
+  onSave(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const {termsAndConditions, ...newPerson} = this.myForm.value; //1
+    this.person = newPerson;
+  }
+
+  isValidField(field: string): boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+ }
+
 }
+
+
+// 1-> Se hace para eliminar la propiedad termsAndConditions porque si se enviara a una BD, esta propiedad no estaría incluid en la BD y daría problemas
+//     newPerson contendrá todas las propiedades menos termsAndConditions. Aparecerán sus datos en el html
